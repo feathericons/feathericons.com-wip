16a82d4d42a38065d78c4d11011d7aab88820098
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-25 02:05:28 +0900

    Fixed Tailwind integration and moved <HeadHome> to _app.js

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+
+import "stylesheets/tailwind-css/tailwind.generated.css"
+
+// TODO: Add <link rel="icon" href="/favicon.ico" />.
+const App = ({ Component, pageProps }) => (
+	<>
+		<Head>
+			<title>
+				Feathericons 2.0
+			</title>
+			<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</>
+)
+
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
-import Head from "next/head"
-
 const Home = () => (
-	<div>
-		<Head>
-		  <title>Create Next App</title>
-			{/* TODO */}
-		  {/* <link rel="icon" href="/favicon.ico" /> */}
-			<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-		</Head>
-
-		Hello, world!
+	<div className="px-4 py-24 flex flex-row justify-center">
+		<p>
+			hello
+		</p>
 	</div>
 )
 
