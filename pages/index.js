7bfcf056f807f3ba24651465f6014fb827b28309
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-25 03:57:56 +0900

    Came up with a pattern to vertically align icons to text baseline

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,64 @@
+import Apply from "lib/x/Apply"
 import Link from "next/Link"
+import Reset from "lib/x/Reset"
 
+import { ExternalLink as ExternalLinkSVG } from "react-feather"
+import { Feather as FeatherSVG } from "react-feather"
+
+// // ??
 // const tw = tw => `${tw * 4}rem`
 
 const Home = () => (
-	<div className="px-4 py-16 flex flex-row justify-center">
+	<div className="px-4 py-16 flex flex-row justify-center bg-gray-100">
 		<div className="flex flex-row justify-between w-full max-w-screen-xl">
 
 			{/* LHS */}
 			<div>
 
 				{/* H1 */}
-				<h1 className="font-bold text-5xl">
-					Feathericons
-				</h1>
+				<div className="flex flex-row items-center">
+					<h1
+						className="font-bold text-5xl"
+						// style={{
+						// 	fontSize: "3rem",
+						// 	lineHeight: 1,
+						// }}
+					>
+						Feathericons
+					</h1>
+					<Apply className="ml-2 w-10 h-10 text-blue-600">
+						<FeatherSVG />
+					</Apply>
+				</div>
 
 				{/* H2 */}
 				<div className="h-4" />
-				<h2 className="font-medium text-lg">
-					feathericons MIT-Licensed Icons by{" "}
-					<a
-						// className="underline"
-						// style={{ textDecorationColor: "var(--blue-500)" }}
-						href="https://twitter.com/colebemis"
-					>
-						Cole Bemis
-					</a>
+				<h2 className="text-xl">
+					<span className="inline-flex flex-row items-center">
+						<Apply className="w-4 h-4 text-blue-600" style={{ strokeWidth: 2.5 }}>
+							<FeatherSVG className="inline-block" />
+						</Apply>
+					</span>{" "}
+					Feathericons by{" "}
+					<span className="inline-flex flex-row items-center">
+						<a className="font-medium text-blue-600" href="https://twitter.com/colebemis">
+							@colebemis{" "}
+							<Apply className="ml-1 w-4 h-4" style={{ strokeWidth: 3.25 }}>
+								<ExternalLinkSVG className="inline-block" />
+							</Apply>
+						</a>
+					</span>
 				</h2>
-				<h2 className="font-medium text-lg">
+				<h2 className="text-xl">
 					feathericons.dev by{" "}
-					<a
-						// className="underline"
-						// style={{ textDecorationColor: "var(--blue-500)" }}
-						href="https://twitter.com/username_ZAYDEK"
-					>
-						Zaydek MG
-					</a>
+					<span className="inline-flex flex-row items-center">
+						<a className="font-medium text-blue-600" href="https://twitter.com/username_ZAYDEK">
+							@username_ZAYDEK{" "}
+							<Apply className="ml-1 w-4 h-4" style={{ strokeWidth: 3.25 }}>
+								<ExternalLinkSVG className="inline-block" />
+							</Apply>
+						</a>
+					</span>
 				</h2>
 
 				{/* CTA */}
@@ -42,8 +66,28 @@ const Home = () => (
 				<div className="-mx-4">
 					<div className="-mx-2 flex flex-row">
 
-						<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
-						<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
+						<Reset className="block">
+							<a className="mx-2 my-1 h-16 bg-white rounded-full">
+								<div className="px-6 flex flex-row justify-center items-center h-full">
+									<div className="mr-3 w-4 h-4 bg-gray-200" />
+									<p className="font-medium">
+										Open in Figma
+									</p>
+									<div className="ml-3 w-4 h-4 bg-gray-200" />
+								</div>
+							</a>
+						</Reset>
+						<Reset className="block">
+							<a className="mx-2 my-1 h-16 bg-white rounded-full">
+								<div className="px-6 flex flex-row justify-center items-center h-full">
+									<div className="mr-3 w-4 h-4 bg-gray-200" />
+									<p className="font-medium">
+										Open in GitHub
+									</p>
+									<div className="ml-3 w-4 h-4 bg-gray-200" />
+								</div>
+							</a>
+						</Reset>
 
 					</div>
 				</div>
@@ -51,26 +95,28 @@ const Home = () => (
 			</div>
 
 			{/* RHS */}
-			<div className="w-full max-w-lg">
+			{/* */}
+			{/* NOTE: Uses -mx-2 to offset sponsors mx-2 */}
+			<div className="-mx-2 w-full max-w-lg">
 
-				<h6 className="text-center font-bold text-xs tracking-wider text-blue-500">
+				<h6 className="text-center font-bold text-xs tracking-wider text-blue-600">
 					OUR SPONSORS
 				</h6>
 
 				{/* Sponsors */}
 				<div className="h-4" />
 				<div className="flex flex-row justify-center flex-wrap">
-					<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-36 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-36 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-36 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
-					<div className="mx-2 my-1 w-44 h-12 bg-gray-100 rounded-full" />
+					<div className="mx-2 my-1 w-44 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-44 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-36 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-36 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-36 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-44 h-12 bg-white rounded-full" />
+					<div className="mx-2 my-1 w-44 h-12 bg-white rounded-full" />
 				</div>
 
 				<div className="h-4" />
-				<h6 className="text-center font-bold text-xs tracking-wider text-blue-500">
+				<h6 className="text-center font-bold text-xs tracking-wider text-blue-600">
 					BECOME A SPONSOR
 				</h6>
 
