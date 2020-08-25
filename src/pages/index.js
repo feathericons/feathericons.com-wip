a17dad14c539f998ce76c0300908d2ed73731747
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-26 07:42:13 +0900

    Changed the CTA colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Apply from "lib/x/Apply"
+import ApplyTransition from "lib/x/ApplyTransition"
 import Link from "next/Link"
 import Reset from "lib/x/Reset"
 
@@ -27,7 +28,7 @@ const Header = () => (
 				<h1 className="text-center font-semibold text-5xl" style={{ letterSpacing: "-0.0125em" }}>
 					<span className="inline-flex flex-row items-baseline">
 						Feathericons{" "}
-						<Apply className="ml-2 w-10 h-10 text-theme" style={{ strokeWidth: 2.25 }}>
+						<Apply className="ml-2 w-10 h-10 text-theme" style={{ strokeWidth: 2.125 }}>
 							<FeatherSVG />
 						</Apply>
 					</span>
@@ -49,52 +50,58 @@ const Header = () => (
 				<div className="!-mx-6 space-x-3 flex flex-row justify-center">
 
 					{/* Figma */}
-					<Reset className="block">
-						{/* style={{ backdropFilter: "brightness(110%) saturate(110%)" }} */}
-						<a className="h-16 bg-blue-100" style={{ borderRadius: "1rem" }} href="TODO">
-							<div className="px-4 flex flex-row justify-center items-center h-full">
-								<Apply className="mr-3 w-5 h-5 text-theme" style={{ strokeWidth: 2.25 }}>
-									<svg viewBox="0 0 38 57">
-										<path fill="#1abcfe" d="M19 28.5a9.5 9.5 0 1 1 19 0 9.5 9.5 0 0 1-19 0z" />
-										<path fill="#0acf83" d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5a9.5 9.5 0 1 1-19 0z" />
-										<path fill="#ff7262" d="M19 0v19h9.5a9.5 9.5 0 1 0 0-19H19z" />
-										<path fill="#f24e1e" d="M0 9.5A9.5 9.5 0 0 0 9.5 19H19V0H9.5A9.5 9.5 0 0 0 0 9.5z" />
-										<path fill="#a259ff" d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" />
-									</svg>
-								</Apply>
-								<p className="text-lg text-theme">
-									<span className="inline-flex flex-row items-center">
-										Open in Figma{" "}
-										<Apply className="ml-1.5 w-4 h-4 text-blue-600 opacity-75" style={{ strokeWidth: 2.25 }}>
-											<ExternalLinkSVG />
-										</Apply>
-									</span>
-								</p>
-							</div>
-						</a>
-					</Reset>
+					{/* */}
+					{/* style={{ backdropFilter: "brightness(110%) saturate(110%)" }} */}
+					<ApplyTransition>
+						<Reset className="block">
+							<a className="h-16 bg-white hover:bg-blue-50" style={{ borderRadius: "1rem" }} href="TODO">
+								<div className="px-4 flex flex-row justify-center items-center h-full">
+									<Apply className="mr-3 w-5 h-5 text-blue-600" style={{ strokeWidth: 2.25 }}>
+										<svg viewBox="0 0 38 57">
+											<path fill="#1abcfe" d="M19 28.5a9.5 9.5 0 1 1 19 0 9.5 9.5 0 0 1-19 0z" />
+											<path fill="#0acf83" d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5a9.5 9.5 0 1 1-19 0z" />
+											<path fill="#ff7262" d="M19 0v19h9.5a9.5 9.5 0 1 0 0-19H19z" />
+											<path fill="#f24e1e" d="M0 9.5A9.5 9.5 0 0 0 9.5 19H19V0H9.5A9.5 9.5 0 0 0 0 9.5z" />
+											<path fill="#a259ff" d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" />
+										</svg>
+									</Apply>
+									<p className="text-lg text-blue-600">
+										<span className="inline-flex flex-row items-center">
+											Open in Figma{" "}
+											<Apply className="ml-1.5 w-4 h-4 text-blue-600 opacity-75" style={{ strokeWidth: 2.25 }}>
+												<ExternalLinkSVG />
+											</Apply>
+										</span>
+									</p>
+								</div>
+							</a>
+						</Reset>
+					</ApplyTransition>
 
 					{/* GitHub */}
-					<Reset className="block">
-						{/* style={{ backdropFilter: "brightness(110%) saturate(110%)" }} */}
-						<a className="h-16 bg-blue-100" style={{ borderRadius: "1rem" }} href="TODO">
-							<div className="px-4 flex flex-row justify-center items-center h-full">
-								<Apply className="mr-3 w-5 h-5 text-theme" style={{ strokeWidth: 2.25 }}>
-									<svg viewBox="0 0 16 16" fill="currentColor">
-										<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-									</svg>
-								</Apply>
-								<p className="text-lg text-theme">
-									<span className="inline-flex flex-row items-center">
-										Open in GitHub{" "}
-										<Apply className="ml-1.5 w-4 h-4 text-blue-600 opacity-75" style={{ strokeWidth: 2.25 }}>
-											<ExternalLinkSVG />
-										</Apply>
-									</span>
-								</p>
-							</div>
-						</a>
-					</Reset>
+					{/* */}
+					{/* style={{ backdropFilter: "brightness(110%) saturate(110%)" }} */}
+					<ApplyTransition>
+						<Reset className="block">
+							<a className="h-16 bg-white hover:bg-blue-50" style={{ borderRadius: "1rem" }} href="TODO">
+								<div className="px-4 flex flex-row justify-center items-center h-full">
+									<Apply className="mr-3 w-5 h-5 text-blue-600" style={{ strokeWidth: 2.25 }}>
+										<svg viewBox="0 0 16 16" fill="currentColor">
+											<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+										</svg>
+									</Apply>
+									<p className="text-lg text-blue-600">
+										<span className="inline-flex flex-row items-center">
+											Open in GitHub{" "}
+											<Apply className="ml-1.5 w-4 h-4 text-blue-600 opacity-75" style={{ strokeWidth: 2.25 }}>
+												<ExternalLinkSVG />
+											</Apply>
+										</span>
+									</p>
+								</div>
+							</a>
+						</Reset>
+					</ApplyTransition>
 
 				</div>
 
