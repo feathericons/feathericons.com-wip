a0ab7be1c5461e99db0dd80ab237ccddff47ca4b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-26 07:32:02 +0900

    Extracted data/sponsors

---
+++ src/data/sponsors.js
@@ -0,0 +1,39 @@
+const sponsors = [
+	{
+		name: "QA Wolf",
+		url: "TOOD",
+		subtext: "open source browser driver",
+	},
+	{
+		name: "Hot Tips Ebook",
+		url: "TOOD",
+		subtext: "designer ebook",
+	},
+	{
+		name: "SkillSyncer",
+		url: "TOOD",
+		subtext: "resumé software",
+	},
+	{
+		name: "codetisans_",
+		url: "TOOD",
+		subtext: "developer studio",
+	},
+	{
+		name: "Streamline",
+		url: "TOOD",
+		subtext: "icons and graphics",
+	},
+	{
+		name: "SPELLL",
+		url: "TOOD",
+		subtext: "Figma spellchecker plugin",
+	},
+	{
+		name: "codex",
+		url: "TOOD",
+		subtext: "open source markdown editor",
+	},
+]
+
+export default sponsors
