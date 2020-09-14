c41179bdb70775e687abe0e9b8665e6bed46d62c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-15 06:38:48 +0900

    Prototyped split sticky concept

--- src/pages/index.js
+++ src/pages/index.js
@@ -238,36 +238,51 @@ function IconApp() {
 	return (
 		// NOTE: Use relative because of #bg-grad.
 		<div className="mt-0 xl:-mt-40 relative flex flex-row justify-center">
-			{/* NOTE: Use items-start because of sticky top-0. */}
-			<main className="w-full max-w-screen-xl">
+			<main className="flex flex-row items-start w-full max-w-screen-xl">
 
-				<div className="mt-0 xl:-mt-4 pt-0 xl:pt-4 sticky top-0 inset-x-0 w-full">
+				{/* NOTE: Use items-start because of sticky top-0. */}
+				{/* <div className=""> */}
 
+				{/* LHS */}
+				<div className="-mt-4 pt-4 sticky top-0 left-0 w-full">
 					{/* #bg-grad */}
 					<div className="absolute top-0 inset-x-0 pointer-events-none" style={{ zIndex: -1 }}>
 						<Style className="w-full h-10">
-							<svg preserveAspectRatio="none" fill="url(#bg-grad)" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+							<svg preserveAspectRatio="none" fill="red" /* fill="url(#bg-grad)" */ viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
 								<rect width={1} height={1} />
 							</svg>
 						</Style>
 					</div>
-					<div className="h-6 bg-white rounded-t-0 xl:rounded-t-6">
-						{/* ... */}
+					<div className="h-20 bg-white rounded-tl-6">
+						SEARCH
 					</div>
 				</div>
 
-				<div className="w-full h-20 bg-red-100">
-					hello
+				{/* RHS */}
+				<div className="-mt-4 pt-4 sticky top-0 right-0 w-80">
+					{/* #bg-grad */}
+					<div className="absolute top-0 inset-x-0 pointer-events-none" style={{ zIndex: -1 }}>
+						<Style className="w-full h-10">
+							<svg preserveAspectRatio="none" fill="red" /* fill="url(#bg-grad)" */ viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+								<rect width={1} height={1} />
+							</svg>
+						</Style>
+					</div>
+
+					{/* <aside> */}
+					<aside className="bg-white rounded-tr-6">
+						<div>
+							ICON
+						</div>
+						<div>
+							ICON
+						</div>
+						<div>
+							ICON
+						</div>
+					</aside>
+
 				</div>
-
-
-				{/* NOTE: Use items-start because of sticky top-0. */}
-				{/* <div className="flex flex-row items-start"> */}
-				{/* 	<div className="w-full h-20 bg-red-100" /> */}
-				{/* 	<div className="w-80 h-80 bg-red-100" /> */}
-				{/* </div> */}
-
-
 
 				<br />
 				<br />
