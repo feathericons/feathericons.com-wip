ab87987e221832878eecb8943dd487d5eb5ec7de
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-29 21:43:23 +0900

    Zip file is now precomputed on the client. This might be disadvantageous for performance. Should review at a later time.

--- src/utils/asyncDownloadAll.js
+++ src/utils/asyncDownloadAll.js
@@ -2,10 +2,13 @@ import download from "downloadjs"
 import Zip from "jszip"
 import { icons } from "feather-icons"
 
-// https://github.com/feathericons/feathericons.com/blob/335d6f5e814eb71e38db4ad4e810ed46f6e3cc3b/src/components/Hero.js
-export default async function asyncDownloadAll() {
+const zip = (async () => {
 	const zip = new Zip()
 	Object.values(icons).forEach(icon => zip.file(`${icon.name}.svg`, icon.toSvg()))
-	const file = await zip.generateAsync({ type: "blob" })
-	download(file, "feather.zip")
+	return await zip.generateAsync({ type: "blob" })
+})()
+
+// https://github.com/feathericons/feathericons.com/blob/335d6f5e814eb71e38db4ad4e810ed46f6e3cc3b/src/components/Hero.js
+export default async function asyncDownloadAll() {
+	await download(zip, "feather.zip")
 }
