09d450b3f12012f90b5b871db9d8f8d00cbb3eba
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-24 12:37:13 +0900

    Prototyped the advanced scroll interaction with the <main> section

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,11 +29,10 @@ import TopRow from "../components/TopRow"
 
 function App() {
 	return (
-		// NOTE: Reset var(--spacing).
-		<div className="hstack sp-0 bg-white xl:rounded-24" style={{ boxShadow: "var(--shadow-xs), var(--shadow-md)" }}>
+		<div className="hstack bg-white xl:rounded-24" style={{ boxShadow: "var(--shadow-xs), var(--shadow-md)" }}>
 			{/* LHS */}
 			<div className="spacer vstack">
-				<div className="sticky t-0">hello</div>
+				<div className="sticky t-0 xl:-mt-24 xl:pt-24">hello</div>
 				<div>a</div>
 				<div>a</div>
 				<div>a</div>
@@ -85,54 +84,8 @@ function App() {
 
 			{/* RHS */}
 			<div className="hidden lg:vstack bg-cool-gray-100" style={{ width: 20 * 16 /* 320px */ }}>
-				<div className="sticky t-0">hello</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
-				<div>a</div>
+				<div className="sticky t-0 xl:-mt-24 xl:pt-24">hello</div>
+				<div className="spacer" />
 			</div>
 		</div>
 	)
@@ -141,20 +94,22 @@ function App() {
 // TODO: bg-cool-gray-50
 export default function Home() {
 	return (
-		<>
-			<div className="vstack sp-96">
-				<div className="absolute t-0 x-0">
-					<div className="px-16 sm:px-24 py-12">
-						<TopRow />
-					</div>
-				</div>
-				<div className="px-16 sm:px-24">
-					<Header />
-				</div>
-				<div className="xl:px-24 xl:pb-24">
-					<App />
+		<div className="vstack sp-96">
+			<div className="absolute t-0 x-0">
+				<div className="px-16 sm:px-24 py-12">
+					<TopRow />
 				</div>
 			</div>
-		</>
+			<div className="px-16 sm:px-24">
+				<Header />
+			</div>
+			{/* NOTE: Resets var(--spacing). */}
+			<div className="sp-0 xl:px-24 xl:pb-24">
+				<div className="hidden xl:block sticky t-0">
+					<div className="h-24 bg-red-100" />
+				</div>
+				<App />
+			</div>
+		</div>
 	)
 }
