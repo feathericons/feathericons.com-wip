d715fcab3d730b6f4a8b6a0e87c88493693993fa
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-24 10:32:33 +0900

    Prototyped responsive header

---
+++ src/components/ExtAnchors.js
@@ -0,0 +1,34 @@
+import { Twitter as SVGTwitter } from "react-feather"
+import target_blank from "../utils/target_blank"
+
+// prettier-ignore
+const TwitterHref = `https://twitter.com/intent/tweet?text=${encodeURI("Check out Feathericons! ✨\n\nMany thanks to @colebemis for Feather Icons and @username_ZAYDEK for feathericons.com. ❤️",)}`
+const GitHubLicenseHref = "https://github.com/feathericons/feather/blob/master/LICENSE"
+const GitHubReleaseHref = "https://github.com/feathericons/feather/releases"
+
+// TODO: Add analytic events.
+export default function ExtAnchors() {
+	return (
+		<div className="font-500">
+			<div className="hstack sp-8 py-12">
+				{/* LHS */}
+				<a className="hstack align-center hover:underline" href={TwitterHref} {...target_blank}>
+					<SVGTwitter style={{ width: "1.25em", height: "1.25em" }} fill="currentColor" strokeWidth={0} />
+					<span>Tweet thank you!</span>
+				</a>
+
+				{/* RHS */}
+				<div className="spacer" />
+				<div className="hstack">
+					<a className="hover:underline" href={GitHubLicenseHref} {...target_blank}>
+						MIT-license
+					</a>
+					<span>&middot;</span>
+					<a className="hover:underline" href={GitHubReleaseHref} {...target_blank}>
+						v4.28
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
