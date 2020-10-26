2c93a019ca304838f4b16bfa7dfa4009767769db
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-26 10:06:12 +0900

    Consolidated <Sponsors>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import { Download as SVGDownload, GitHub as SVGGitHub } from "react-feather"
 
-import Sponsors from "./Sponsors"
 import useBreakpoints from "../utils/useBreakpoints"
 
 // TODO: Add anchors and analytics.
@@ -37,6 +36,69 @@ function HeaderLHS() {
 				</div>
 			</div>
 		</div>
+	)
+}
+
+function Sponsor() {
+	return (
+		<div className="vstack s-8">
+			<div className="h-56 bg-cool-gray-100 rounded-full" style={{ width: `${176 / 16}rem` }} />
+			{/* prettier-ignore */}
+			<div className="font-500 text-center text-12 leading-1">
+				Placeholder
+			</div>
+		</div>
+	)
+}
+
+// TODO: Add sponsors.
+function Sponsors() {
+	const screen = useBreakpoints()
+	return (
+		<>
+			{screen.xs && !screen.xl && (
+				<div className="hstack -m-s-1/2 pt-s" style={{ flexWrap: "wrap" }}>
+					<div className="m-s-1/2">
+						<Sponsor />
+					</div>
+					<div className="m-s-1/2">
+						<Sponsor />
+					</div>
+					<div className="m-s-1/2">
+						<Sponsor />
+					</div>
+					<div className="m-s-1/2">
+						<Sponsor />
+					</div>
+					<div className="m-s-1/2">
+						<Sponsor />
+					</div>
+				</div>
+			)}
+			{screen.xl && (
+				<>
+					<div className="hstack -m-s-1/2 pt-s">
+						<div className="m-s-1/2">
+							<Sponsor />
+						</div>
+						<div className="m-s-1/2">
+							<Sponsor />
+						</div>
+						<div className="m-s-1/2">
+							<Sponsor />
+						</div>
+					</div>
+					<div className="hstack -m-s-1/2 pt-s">
+						<div className="m-s-1/2">
+							<Sponsor />
+						</div>
+						<div className="m-s-1/2">
+							<Sponsor />
+						</div>
+					</div>
+				</>
+			)}
+		</>
 	)
 }
 
