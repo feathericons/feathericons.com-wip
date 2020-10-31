fbdff67b0b409a03c66b2c8995b6e00acf635050
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-31 23:31:20 +0900

    Missed a file

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -1,6 +1,6 @@
 export const hrefs = {
 	shareOnTwitter: `https://twitter.com/intent/tweet?text=${encodeURI(
-		"Check out Feathericons! ✨\n\nMany thanks to @colebemis for Feather Icons and @username_ZAYDEK for feathericons.com. ❤️",
+		"Check out Feathericons! ✨\n\nThanks @colebemis for Feather Icons and @username_ZAYDEK for the new feathericons.com. ❤️",
 	)}`,
 	GitHubLicense: "https://github.com/feathericons/feather/blob/master/LICENSE",
 	GitHubRelease: "https://github.com/feathericons/feather/releases",
