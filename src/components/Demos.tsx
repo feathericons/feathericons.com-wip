43586357428fd0ebb79f256a1b321e375d99583d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-01 15:34:32 +0900

    Added <Demo5>

--- src/components/Demos.tsx
+++ src/components/Demos.tsx
@@ -1,5 +1,13 @@
 import React from "react"
-import { GitHub as SVGGitHub } from "react-feather"
+import {
+	GitHub as SVGGitHub,
+	Heart as SVGHeart,
+	Instagram as SVGInstagram,
+	Mail as SVGMail,
+	MessageCircle as SVGMessageCircle,
+	Send as SVGSend,
+	Twitter as SVGTwitter,
+} from "react-feather"
 
 interface DemoWrapperProps {
 	children?: React.ReactNode
@@ -8,7 +16,10 @@ interface DemoWrapperProps {
 function DemoWrapper({ children }: DemoWrapperProps) {
 	return (
 		<div className="hstack bg-gray-100" style={{ height: `${160 / 16}rem` }}>
-			<div className="vstack">{children}</div>
+			{/* prettier-ignore */}
+			<div className="vstack">
+				{children}
+			</div>
 		</div>
 	)
 }
@@ -34,9 +45,48 @@ export function Demo3() {
 export function Demo4() {
 	return <DemoWrapper>Hello</DemoWrapper>
 }
+
+// TODO: Add 15, etc. to w- and h-.
 export function Demo5() {
-	return <DemoWrapper>Hello</DemoWrapper>
+	return (
+		<DemoWrapper>
+			<div className="hstack s-20">
+				<div className="hstack s-8">
+					<SVGHeart className="w-16 h-16 text-gray-800" />
+					<div className="vstack">
+						<div className="text-15 leading-1">31</div>
+					</div>
+				</div>
+				<div className="hstack s-8">
+					<SVGSend className="w-16 h-16 text-gray-800" />
+					<div className="vstack">
+						<div className="text-15 leading-1">41</div>
+					</div>
+				</div>
+				<div className="hstack s-8">
+					<SVGMessageCircle className="w-16 h-16 text-gray-800" />
+					<div className="vstack">
+						<div className="text-15 leading-1">59</div>
+					</div>
+				</div>
+				<div className="hstack s-8">
+					<SVGGitHub className="w-16 h-16 text-gray-800" />
+				</div>
+			</div>
+		</DemoWrapper>
+	)
 }
+
+// TODO: Add brand colors for hover.
 export function Demo6() {
-	return <DemoWrapper>Hello</DemoWrapper>
+	return (
+		<DemoWrapper>
+			<div className="hstack s-20">
+				<SVGGitHub className="w-20 h-20 text-gray-800" />
+				<SVGInstagram className="w-20 h-20 text-gray-800" />
+				<SVGMail className="w-20 h-20 text-gray-800" />
+				<SVGTwitter className="w-20 h-20 text-gray-800" />
+			</div>
+		</DemoWrapper>
+	)
 }
