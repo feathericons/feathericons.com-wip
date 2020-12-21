7110117eefac4ddc4fe61a299b91018e60c3023b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-21 23:27:03 +0900

    Fixed build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,18 @@
 const { build } = require("esbuild")
+const { checkAsync } = require("./utils")
 const markdownItPlugin = require("./markdown-it-plugin")
 
 ;(async () => {
-	// prettier-ignore
-	const [err] = await checkAsync(build({
-		bundle: true,
-		define: { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") },
-		entryPoints: ["src/index.tsx"],
-		minify: process.env.NODE_ENV === "production",
-		outfile: "build/script.js",
-		plugins: [markdownItPlugin],
-	}))
+	const [_, err] = await checkAsync(
+		build({
+			bundle: true,
+			define: { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") },
+			entryPoints: ["src/index.tsx"],
+			minify: process.env.NODE_ENV === "production",
+			outfile: "build/script.js",
+			plugins: [markdownItPlugin],
+		}),
+	)
 	if (err) {
 		throw err
 	}
