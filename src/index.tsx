8c7f5b212d153a60ceaafa9c6c504f218334333c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-25 16:50:53 +0900

    Fixed smooth dark mode?
    Pretty sure the bug is that when we render so many icons, we bog down Chrome and it creates a faster-perceived repaint.

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom"
 import routes from "./routes"
 import { BrowserRouter as Router } from "react-router-dom"
 
-if (process.env.NODE_ENV === "development" || !routes[window.location.pathname]) {
-	console.log("[Zazen] ReactDOM.render")
+if (__DEV__ === "development" || !routes[window.location.pathname]) {
+	console.log("[debug] ReactDOM.render")
 	ReactDOM.render(
 		<Router>
 			<App />
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "development" || !routes[window.location.pathname])
 		document.getElementById("root"),
 	)
 } else {
-	console.log("[Zazen] ReactDOM.hydrate")
+	console.log("[debug] ReactDOM.hydrate")
 	ReactDOM.hydrate(
 		<Router>
 			<App />
