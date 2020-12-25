b57e42690690ae728599707046c5126fdea9f6bc
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-25 16:00:08 +0900

    Upgraded prerender CSS

--- scripts/prerender-css.go
+++ scripts/prerender-css.go
@@ -4,50 +4,19 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
-	"regexp"
 	"strings"
-	"sync"
+
+	"golang.org/x/sync/errgroup"
 )
 
-const (
-	sass_cmd     = `yarn --silent Sass --load-path=node_modules --no-source-map --style=compressed %[1]s %[2]s`
-	prettier_cmd = `yarn prettier --write %[1]s %[2]s`
-)
-
-// Panics on non-nil errors.
-func check(desc string, err error) {
-	if err == nil {
-		// No-op
-		return
-	}
-	errf := fmt.Errorf("%s: %s", desc, err)
-	panic(errf)
-}
-
-// Ex: microsoft.word -> microsoft
+// Ex: ms.word -> ms
 func basename(pathstr string) string {
 	base := path.Base(pathstr)
 	return base[:len(base)-len(path.Ext(base))]
-}
-
-func glob(pathstr string, pattern *regexp.Regexp) ([]string, error) {
-	matches, err := filepath.Glob(path.Join(path.Clean(pathstr+""), "*"))
-	if err != nil {
-		return nil, err
-	}
-	submatches := []string{}
-	for _, m := range matches {
-		if !pattern.MatchString(m) || strings.Contains(m, "TODO") {
-			// No-op
-			continue
-		}
-		submatches = append(submatches, m)
-	}
-	return submatches, nil
 }
 
 func execCmd(cmdStr string) (stdout, stderr string, err error) {
@@ -60,24 +29,34 @@ func execCmd(cmdStr string) (stdout, stderr string, err error) {
 	return stdoutBuf.String(), stderBuf.String(), cmd.Run()
 }
 
-func Sass(inFile, outFile string) (inBytes int64, outBytes int64, err error) {
+func Sass(inFile, outFile string) (err error) {
+	cmdStr := `yarn --silent sass --load-path=node_modules --no-source-map --style=compressed %[1]s %[2]s`
+	stdout, stderr, _ := execCmd(fmt.Sprintf(cmdStr, inFile, outFile))
+	if stderr != "" {
+		err := errors.New(stderr)
+		return err
+	}
+	fmt.Print(stdout)
+	return nil
+}
+
+func Prettier(inFile, outFile string) error {
+	cmdStr := `yarn --silent prettier --write %[1]s %[2]s`
+	stdout, stderr, _ := execCmd(fmt.Sprintf(cmdStr, inFile, outFile))
+	if stderr != "" {
+		err := errors.New(stderr)
+		return err
+	}
+	fmt.Print(stdout)
+	return nil
+}
+
+func bytesize(inFile, outFile string) (inBytes, outBytes int64, err error) {
 	inInfo, err := os.Stat(inFile)
 	if err != nil {
 		return 0, 0, err
 	}
 	inBytes = inInfo.Size()
-	sass_stdout, sass_stderr, _ := execCmd(fmt.Sprintf(sass_cmd, inFile, outFile))
-	if sass_stderr != "" {
-		err := errors.New(sass_stderr)
-		return 0, 0, err
-	}
-	fmt.Print(sass_stdout)
-	prettier_stdout, prettier_stderr, _ := execCmd(fmt.Sprintf(prettier_cmd, inFile, outFile))
-	if prettier_stderr != "" {
-		err := errors.New(prettier_stderr)
-		return 0, 0, err
-	}
-	fmt.Print(prettier_stdout)
 	outInfo, err := os.Stat(outFile)
 	if err != nil {
 		return 0, 0, err
@@ -86,27 +65,36 @@ func Sass(inFile, outFile string) (inBytes int64, outBytes int64, err error) {
 	return inBytes, outBytes, nil
 }
 
+// Ex: go run scripts/prerender-css.go src/stylesheets/*.scss
 func main() {
 	if len(os.Args) < 2 {
-		panic("no args; try go run ... src/stylesheets")
+		log.Fatal("no args; try go run ... src/stylesheets")
 	}
-
-	matches, err := glob(os.Args[1], regexp.MustCompile(`\.s?css$`))
-	if err == nil && len(matches) == 0 {
-		err = errors.New("no matches")
-	}
-	check("glob", err)
-	wg := &sync.WaitGroup{}
-	for _, m := range matches {
-		wg.Add(1)
-		go func(m string) {
-			defer wg.Done()
-			inFile := m
-			outFile := "build/stylesheets/" + basename(m) + ".css"
-			inBytes, outBytes, err := Sass(inFile, outFile)
-			check("Sass", err)
+	cmdArgs := os.Args[1:]
+	g := new(errgroup.Group)
+	for _, arg := range cmdArgs {
+		arg := arg
+		g.Go(func() error {
+			inFile := arg
+			outFile := fmt.Sprintf("build/stylesheets/%s.css", basename(arg))
+			err1 := Sass(inFile, outFile)
+			if err1 != nil {
+				return err1
+			}
+			err2 := Prettier(inFile, outFile)
+			if err2 != nil {
+				return err2
+			}
+			inBytes, outBytes, err3 := bytesize(inFile, outFile)
+			if err3 != nil {
+				return err3
+			}
 			fmt.Printf("✅ %s (%dkB) -> %s (%dkB)\n", inFile, inBytes/1e3, outFile, outBytes/1e3)
-		}(m)
+			return nil
+		})
 	}
-	wg.Wait()
+	err := g.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
